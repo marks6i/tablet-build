83574d99e454f58d7be12c0ca39722e592456137
Mark Solinski <coach.marks6i@gmail.com>
2023-03-15 14:43:31 -0700

    First attempt to use script

--- install.sh
+++ install.sh
@@ -1,51 +1,25 @@
-ls
-sudo apt update && sudo apt upgrade
-sudo apt-get install openssh-server
-sudo apt install vim
-sudo apt install software-properties-common
+#!/usr/bin/env bash
+
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt install vim build-essential python3-dev manpages-dev
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
+
 sudo apt install python3.11
 sudo apt install python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
-python3 --version
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-python --version
+
 mkdir .venv
 cd .venv
-which python
-ls /usr/bin
-python3.11 --version
 python3.11 -m venv PythonData
-source PythonData/bin/activate
-python --version
+source ~/.venv/PythonData/bin/activate
+
 python -m pip install --upgrade pip
 pip install pandas matplotlib
-pip install jupyter
-sudo pip install psutil
-sudo apt-get install gcc python3-dev
 sudo pip install psutil
 pip install jupyter
-cd
-ls
-python --version
-jupyter notebook
-exit
-sudo apt install build-essentials
-sudo apt install build-essential
-sudo apt update && sudo apt upgrade
-sudo apt install git
-exit
-ls
-./.venv/PythonData/bin/activate
-.venv/PythonData/bin/activate
-sudo .venv/PythonData/bin/activate
-deactivate
-exit
-python --version
-source .venv/PythonData/bin/activate
-ls
-sudobapt update && sudo apt upgrade
-sudo apt update && sudo apt upgrade
 pip install requests
 sudo apt install sqlite3
 pip install numpy
@@ -55,63 +29,6 @@ pip install pendulum
 pip install pillow
 pip install scikit-learn
 pip install flask
-exit
-source .venv/PythonData/bin/activate
-pip install csv random json 
-pip install  random json 
-pip install json
-vim .bashrc
-pip install --upgrade pip
-pip install json
-pip install sqlalchemy
-pip install pvplot
-pip install hvplot
-sudo apt update && sudo apt upgrade
-exit
-sudo apt install dirmngr gnupg apt-transport-http# ca-certificates software-properties-common
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-sudo apt-key adv --keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-wget -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/cran.gpg
-cat /usr/share/keyrings/cran.gpg
-echo deb [signed-by=/usr/share/keyrings/cran.gpg] https://cloud.r-project.org/bin/linux/ubuntu
 
-rm /usr/share/keyrings/cran.gpg
-wget -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-echo sudo apt install --no-install-recommends r-base
-sudo apt update && sudo apt upgrade
-sudo apt install --no-install-recommends r-base
-sudo apt install r-base-core r-recommended r-base
-hostnamectl
-sudo apt install --no-install-recommends r-base
-sudo apt update && sudo apt upgrade
-sudo apt dist-upgrade
-sudo apt-get purge r-base* r-rexommended r-cran-*
-ls
-rm *
-ls
-history -w install.sh
-less install.sh 
-git clone https://github.com/marks6i/table-build.git
-git config --global user.name "Mark Solinski"
-git config --global user.email "coach.marks6i@gmail.com"
-git clone https://github.com/marks6i/table-build.git
-cd .ssh
-ssh-keygen -t ed25519 -C "coach.marks6i.gmail.com"
-eval "$(ssh-agent -s)"
-git clone https://github.com/marks6i/table-build.git
-git clone git@github.com:marks6i/table-build.git
-ls
-git clone git@github.com:marks6i/tablet-build.git
-ls
-cd .ssh
-cat
-ls
-cat id_ed25519.pub 
-cd
-git clone git@github.com:marks6i/tablet-build.git
-ls
-rm install.sh 
-cd tablet-build/
-history -w install.sh
+pip install sqlalchemy
+pip install hvplot

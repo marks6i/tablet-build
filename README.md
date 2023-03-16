67fadd7cc8b1fa4d02b186edcc4525e829ad4747
Mark Solinski <coach.marks6i@gmail.com>
2023-03-15 23:10:25 -0700

    new try

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
 # tablet-build
 Code to help build my tablet's Userland instance
-
-sudo apt update && sudo apt upgrade
-sudo apt-get install openssh-server
-sudo apt install git
-git config --global user.name "Mark Solinski"
-git config --global user.email "coach.marks6i@gmail.com"
-ssh-keygen -t ed25519 -C "coach.marks6i.gmail.com"
-cd .ssh
-eval "$(ssh-agent -s)"
-cat id_ed25519.pub
-
-copy the public key to github so the code below works...
-
-git clone https://github.com/marks6i/table-build.git
--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-sudo apt install vim build-essential python3-dev manpages-dev
+sudo apt install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt install -y vim build-essential python3-dev manpages-dev
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 
-sudo apt install python3.11
-sudo apt install python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
+sudo apt install -y python3.11 python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm python3.11-tk python3.11-lib2to3
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 
 mkdir .venv
 cd .venv
 python3.11 -m venv PythonData
-source ~/.venv/PythonData/bin/activate
+source PythonData/bin/activate
 
 python -m pip install --upgrade pip
 pip install pandas matplotlib
